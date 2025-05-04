bd168d803675382603abb3d24465bcb021fd119a
cfojas825 <clarkjames8205@gmail.com>
2025-05-03 18:28:26 -0700

    Update npc.cpp

--- npc.cpp
+++ npc.cpp
@@ -4,8 +4,7 @@
 
 NPC::NPC(std::string name, int x, int y, std::string questDesc, int xp)
     : name(name), posX(x), posY(y), questDescription(questDesc),
-      questAccepted(false), questCompleted(false), rewardXP(xp) {
-}
+      questAccepted(false), questCompleted(false), rewardXP(xp) {}
 
 int NPC::getX() const {
     return posX;
@@ -28,18 +27,22 @@ void NPC::interact(Player& player) {
             std::cout << "Invalid input.\n";
             return;
         }
+
         if (choice == 1) {
             questAccepted = true;
             std::cout << name << ": Good luck... You’ll need it.\n";
         } else {
             std::cout << name << ": Maybe next time.\n";
         }
-    } else if (!questCompleted && player.getMonstersKilled() >= 1) {
-        questCompleted = true;
-        std::cout << name << ": You have done well. Here is your reward.\n";
-        player.gainXP(rewardXP);
     } else if (!questCompleted) {
-        std::cout << name << ": Return once you have slain 1 monster.\n";
+        // Example quest: kill at least 1 monster
+        if (player.getMonstersKilled() >= 1) {
+            questCompleted = true;
+            std::cout << name << ": You have done well. Here is your reward.\n";
+            player.gainXP(rewardXP);
+        } else {
+            std::cout << name << ": Return once you have slain 1 monster.\n";
+        }
     } else {
         std::cout << name << ": Thank you again for your help.\n";
     }
