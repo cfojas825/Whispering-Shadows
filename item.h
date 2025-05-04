494f9a7aa6d41575e6ce6dd5d8c4c271c4e51973
cfojas825 <clarkjames8205@gmail.com>
2025-05-03 18:26:06 -0700

    Update item.h

--- item.h
+++ item.h
@@ -10,14 +10,13 @@ struct Item {
     std::string name;
     std::string description;
     ItemType type;
-    int effectAmount;   // e.g. HP restored
-    int value;          // currency or power
-    Rarity rarity;      // Common, Rare, Epic
+    int effectAmount;   
+    int value;         
+    Rarity rarity;
 
     Item(std::string n = "", std::string d = "", ItemType t = ItemType::POTION,
-        int amt = 0, int val = 0, Rarity r = Rarity::COMMON)
-        : name(n), description(d), type(t), effectAmount(amt), value(val), rarity(r) {
-    }
+         int amt = 0, int val = 0, Rarity r = Rarity::COMMON)
+        : name(n), description(d), type(t), effectAmount(amt), value(val), rarity(r) {}
 };
 
 #endif // ITEM_H
